6b8813fe91bd7047f98267db42f1da4abe14791e
Jhader Manuel Hurtado Castro <jmhurtadoc@unal.edu.co>
2019-09-28 12:32:41 -0500

    Jader cambioas

---
+++ src/main/java/com/velocidevtors/hospital/ClasificacionHospitalServerApplication.java
@@ -0,0 +1,13 @@
+package com.velocidevtors.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasificacionHospitalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasificacionHospitalServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/velocidevtors/hospital/ClasificacionHospitalServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.velocidevtors.hospital;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClasificacionHospitalServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
